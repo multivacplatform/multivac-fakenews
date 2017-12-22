6fecb2334622251ff36443d742cfa3148a54c6c1
Maziyar PANAHI <maziyar.panahi@iscpif.fr>
2017-12-22 14:24:33 +0100

    load data

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,12 @@
+import data_processing.DataFrame_Builder
+
 object Main {
   def main(args: Array[String]) {
     val env =args(0)
     val spark = SessionBuilder.buildSession(env)
+
+    val (debunkDF, hoaxDF) = DataFrame_Builder.loadData(env, spark, "./data/decodex/debunk_lines.json", "./data/decodex/hoax_lines.json", "json")
+
 
   }
 }
