a5e924992b7f311992c859d87fb10714e6e03b60
Maziyar PANAHI <maziyar.panahi@iscpif.fr>
2017-12-22 13:22:02 +0100

    session builder (local and prod)

---
+++ src/main/scala/SessionBuilder.scala
@@ -0,0 +1,21 @@
+import com.typesafe.config.ConfigFactory
+import org.apache.spark.sql.SparkSession
+
+object SessionBuilder {
+  def buildSession(env: String): SparkSession = {
+
+    var sparkMaster = ConfigFactory.load().getString("spark.local.master.value")
+    if(env == "prod")
+      sparkMaster = ConfigFactory.load().getString("spark.prod.master.value")
+
+    val spark: SparkSession = SparkSession.builder
+      .appName("multivac-fakenews")
+      .master(sparkMaster)
+      .enableHiveSupport()
+      .getOrCreate
+
+    spark.sparkContext.setLogLevel("WARN")
+
+    spark
+  }
+}
