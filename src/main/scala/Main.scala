6e55f84efd78cdf8bf186ee3e01a64faf45be379
Maziyar PANAHI <maziyar.panahi@iscpif.fr>
2017-12-24 17:03:27 +0100

    read files path from app conf

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,17 @@
+import com.typesafe.config.ConfigFactory
 import data_processing.DataFrame_Builder
 
 object Main {
   def main(args: Array[String]) {
     val env =args(0)
     val spark = SessionBuilder.buildSession(env)
+    val hoaxJSONFilePath = ConfigFactory.load().getString("data.decodexHoax")
+    val debunkJSONFilePath = ConfigFactory.load().getString("data.decodexHoax")
 
-    val (debunkDF, hoaxDF) = DataFrame_Builder.loadData(env, spark, "./data/decodex/debunk_lines.json", "./data/decodex/hoax_lines.json", "json")
 
+    val (debunkDF, hoaxDF) = DataFrame_Builder.loadData(
+      env, spark, debunkJSONFilePath, hoaxJSONFilePath, "json"
+    )
 
   }
 }
